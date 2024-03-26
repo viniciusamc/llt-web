cc67ab2091800e59a749c5a7aeda04e4eed5314a
Vinicius <viniciusmartinsdacunha1@gmail.com>
2024-03-26 01:30:05 -0300

    feat:token activation

---
+++ src/pages/Token/index.jsx
@@ -0,0 +1,54 @@
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import { api } from '../../services/api';
+import { useState } from 'react';
+import { Flash } from '../../components/Flash';
+
+export function Token() {
+    const { token } = useParams();
+
+    const [status, setStatus] = useState();
+    const [errorMessage, setErrorMessage] = useState();
+
+    useEffect(() => {
+        async function activateAccount() {
+            try {
+                const response = await api.get(`/v1/users/token/${token}`);
+
+                if (response.status === 201) {
+                    setStatus(200)
+                    setTimeout(() => {
+                        window.location.href = '/';
+                    }, 5000);
+                }
+            } catch (error) {
+                setErrorMessage('Was not possible to active your account, please try again');
+                console.error(error)
+            }
+        }
+
+        activateAccount();
+    }, [token]);
+
+    return (
+        <main
+            style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                height: '90vh',
+                margin: '0 auto',
+                textAlign: 'center',
+            }}
+        >
+        <Flash level={3} message={errorMessage} />
+            {status && (
+                <>
+                    <h4>Your account has been activated. You can now sign in.</h4>
+                    <h1>Thanks.</h1>
+                </>
+            )}
+        </main>
+    );
+}
--- src/routes/app.routes.jsx
+++ src/routes/app.routes.jsx
@@ -2,11 +2,13 @@ import { Routes, Route } from 'react-router';
 
 import { SignUp } from '../pages/SignUp';
 import { Login } from '../pages/Login';
+import { Token } from '../pages/Token';
 
 export function AppRoutes() {
     return (
         <Routes>
             <Route path="/signup" element={<SignUp />} />
+            <Route path="/token/:token" element={<Token />} />
             <Route path="/" element={<Login />} />
         </Routes>
     );
