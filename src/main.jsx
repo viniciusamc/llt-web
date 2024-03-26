8549d90d227c81a3200445112ca954117eaec2a5
Vinicius <viniciusmartinsdacunha1@gmail.com>
2024-03-26 02:24:55 -0300

    hotfix: vercel trackers

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,6 @@ import './index.css';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme.js';
 
-import { SpeedInsights } from '@vercel/speed-insights/next';
-import { Analytics } from '@vercel/analytics/react';
-
 import { AuthProvider } from './hooks/auth.jsx';
 import { Routes } from './routes/index.jsx';
 
@@ -14,8 +11,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
             <AuthProvider>
-                <SpeedInsights />
-                <Analytics />
                 <Routes />
             </AuthProvider>
         </ThemeProvider>
