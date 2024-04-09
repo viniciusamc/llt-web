721aa6a35cd1ffdce017aa6d4582f977de7db4a4
Vinicius <viniciusmartinsdacunha1@gmail.com>
2024-04-09 05:20:12 -0300

    fix:react query

--- src/main.jsx
+++ src/main.jsx
@@ -7,17 +7,11 @@ import theme from './styles/theme.js';
 import { AuthProvider } from './hooks/auth.jsx';
 import { Routes } from './routes/index.jsx';
 
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-const queryClient = new QueryClient({})
-
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <ThemeProvider theme={theme}>
             <AuthProvider>
-                <QueryClientProvider client={queryClient}>
-                    <Routes />
-                </QueryClientProvider>
+                <Routes />
             </AuthProvider>
         </ThemeProvider>
     </React.StrictMode>,
