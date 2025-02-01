84efd7545d57df4a8aa64e66b4ec03efbf52cf9c
Vinicius Martins <92201792+viniciusamc@users.noreply.github.com>
2025-02-01 12:38:44 -0300

    vite port

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,8 @@ export default defineConfig({
     plugins: [react()],
     optimizeDeps: {
         exclude: ['js-big-decimal']
-    }
+    },
+    server: {
+        port: parseInt(process.env.VITE_PORT) || 5173,
+      },
 });
